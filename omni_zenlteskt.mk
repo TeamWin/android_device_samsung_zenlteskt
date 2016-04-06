1e30c088c07ebecd999d44b799ca7ffe33c84b1c
James Christopher Adduono <jc@adduono.com>
2016-04-05 22:20:12 -0400

    Initialize device tree for zenlteskt

---
+++ omni_zenlteskt.mk
@@ -0,0 +1,14 @@
+# Release name
+PRODUCT_RELEASE_NAME := zenlteskt
+
+$(call inherit-product, build/target/product/embedded.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := zenlteskt
+PRODUCT_NAME := omni_zenlteskt
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-G928S
+PRODUCT_MANUFACTURER := samsung
